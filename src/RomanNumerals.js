d4b94b3b70b645324ddc4f0230cf90b88eaa5fc1
Ruben <ruben@ruben-Aspire-6530G.(none)>
2015-02-26 10:41:49 +0100

    Create repo Roman Numerals Kata

---
+++ src/RomanNumerals.js
@@ -0,0 +1,24 @@
+function RomanNumerals() {
+}
+
+/*
+FizzBuzz.prototype.number = function(n) {
+ 	return 1;
+};
+*/
+/*
+Player.prototype.pause = function() {
+  this.isPlaying = false;
+};
+
+Player.prototype.resume = function() {
+  if (this.isPlaying) {
+    throw new Error("song is already playing");
+  }
+
+  this.isPlaying = true;
+};
+
+Player.prototype.makeFavorite = function() {
+  this.currentlyPlayingSong.persistFavoriteStatus(true);
+};*/
